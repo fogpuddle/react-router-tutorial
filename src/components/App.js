034db7a1288ab5483352dc59498d2fab84a6df67
David Walters <fogpuddle@gmail.com>
2025-11-19 16:20:33 -0800

    routing-basics

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,19 @@
-import logo from "../assets/red30-tech-logo.png";
+import { Routes, Route } from "react-router-dom";
+import Header from "./Header";
+import Home from "./Home";
+import Categories from "./Categories";
+import About from "./About";
 
 function App() {
   return (
     <div className="app">
-      <header className="container">
-        <img
-          className="logo"
-          src={logo}
-          alt="Red30 Tech logo"
-          title="Red30 Tech | Home"
-        />
-      </header>
+      <Header />
+
+      <Routes>
+        <Route path="/" element={<Home title="Welcome to Red30 Tech" />} /> 
+        <Route path="categories" element={<Categories />} />
+        <Route path="about" element={<About />} />
+      </Routes>
 
       <footer className="container">
         &copy;2022 | <a href="https://red30tech.com/">Red30 Tech</a>
