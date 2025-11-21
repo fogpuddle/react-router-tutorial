509e0aeddf70b35068bb0ff825398accb103b69e
David Walters <fogpuddle@gmail.com>
2025-11-21 14:23:30 -0800

    React Routes Extended

--- src/components/Confirmation.js
+++ src/components/Confirmation.js
@@ -1,8 +1,18 @@
+import { useLocation } from "react-router-dom";
+
 export default function Confirmation() {
+  const { state } = useLocation(null);
+
   return (
     <div className="container">
       <h1>Thank You!</h1>
-      <p>You're now registered for Red30 Tech.</p>
+      
+      {state?.name && state?.email && (
+        <>
+          <p>{state.name}, you're now registered for Red30 Tech.</p>
+          <p>We've sent more details to <strong>{state.email}</strong></p>
+        </>
+      )}
     </div>
   );
 }
